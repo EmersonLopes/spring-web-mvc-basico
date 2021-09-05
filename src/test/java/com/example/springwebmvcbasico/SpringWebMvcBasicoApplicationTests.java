410ed6b0a79e9f8b391c66d797fb19094ce547cf
Emerson Lopes <emersonsilvalopes1@gmail.com>
2021-09-05 13:55:12 -0300

    first commit

---
+++ src/test/java/com/example/springwebmvcbasico/SpringWebMvcBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebmvcbasico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMvcBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
